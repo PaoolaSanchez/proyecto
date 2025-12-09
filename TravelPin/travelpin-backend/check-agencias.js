b2a5f40c4e531af2b427a8a7043d759390739fb1
PaoolaSanchez <dania1901@outlook.es>
2025-12-09 02:52:45 -0700

    Delete check-agencias.js

--- TravelPin/travelpin-backend/check-agencias.js
+++
@@ -1,9 +0,0 @@
-const db = require('better-sqlite3')('BDTravelPin.db');
-
-console.log('=== Estructura de tabla agencias ===');
-const info = db.pragma('table_info(agencias)');
-console.log(info);
-
-console.log('\n=== Agencias en la BD ===');
-const agencias = db.prepare('SELECT * FROM agencias').all();
-console.log(agencias);
